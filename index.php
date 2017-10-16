8bd7d6bc5ac68e0c4b6d149f3f75faedf07872b9
PatricNox <sontorp@hotmail.com>
2017-10-16 12:56:46 +0200

    Setup folder structure

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+    // General Configurement
+    declare(strict_types=1);
+    require __DIR__."/include/functions.php";   // PHP Functions
+    require __DIR__."/configs/config.php";      // Database
+
+?>
