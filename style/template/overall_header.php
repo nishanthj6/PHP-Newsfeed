d9597fa9076ac5dcb0c8928fd57530449dc0f53f
PatricNox <sontorp@hotmail.com>
2017-10-16 15:34:51 +0200

    [CSS] Design Overhaul: Default Style

--- style/template/overall_header.php
+++ style/template/overall_header.php
@@ -4,6 +4,17 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="style/theme/stylesheet.css">
     <title><?= $PageTitle ?></title>
 </head>
 <body>
+<div id="Container">
+		<h1 id="Header"></h1>
+		<ol id="Navigation">
+			<li><a href="#" id="on">Home</a></li>
+			<li><a href="#">Information</a></li>
+			<li><a href="#">Chat</a></li>
+			<li><a href="#">Contact Us</a></li>
+			<li><a href="#">Facebook</a></li>
+		</ol>
+		<div class="Clear"></div>
