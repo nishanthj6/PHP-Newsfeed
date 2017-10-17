c48388995dc6a78696c4d5e730b9c305737310c2
PatricNox <sontorp@hotmail.com>
2017-10-17 16:15:06 +0200

    [CSS/PHP] Implement Like System

--- include/functions.php
+++ include/functions.php
@@ -11,3 +11,13 @@ function AssignToTemplate (String $fileName, String $title): string
     if ($Assigned && $PageTitle === $title)
         return $PageTitle;
 }
+
+function LikeSystem ()
+{
+    $VOTEUP     = 'style\\template\\images\\include\\thumb_up1600.png';
+    $VOTEDOWN   =' style\\template\\images\\include\\thumb_down1600.png';
+
+    echo "<span id='thumbs'>";
+    echo "<a href='#'><img src=$VOTEUP /> </a>" . "<a href='#'><img src=$VOTEDOWN /> </a>";
+    echo "</span>";
+}
