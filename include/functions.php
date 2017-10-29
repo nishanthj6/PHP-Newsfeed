303f45cc40d00a1cc9e2d4cfbfbfe2b5a995fe21
PatricNox <sontorp@hotmail.com>
2017-10-29 21:56:01 +0100

    Convert last commit for proper usage

--- include/functions.php
+++ include/functions.php
@@ -4,7 +4,7 @@
 /********************* */
 
 try {
-    require (FEED_ROOT . "configs\\config.php");
+    require (FEED_ROOT . 'configs\config.php');
     $MySQL = new PDO("mysql:host=$DB_HOST;dbname=$DB_NAME", $DB_USER, $DB_PASS, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
 }
 
